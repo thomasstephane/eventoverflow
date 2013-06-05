f31da108642ff12479182f7712425e41b23947e1
thomasstephane <thomas.stephn@gmail.com>
2013-06-05 11:49:22 -0700

    created signup form for user worked on create/new routes for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,15 @@
 class UsersController < ApplicationController
   def create
+    @user = User.new params[:user]
+    if @user.save
+      p 'yeah'
+    end
 
+    redirect_to user_path(@user)
   end
 
   def new
-    
+    @user = User.new
   end
 
   def edit
