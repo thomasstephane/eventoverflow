e05302318265791e52311b8decc05e054c1c1409
thomasstephane <thomas.stephn@gmail.com>
2013-06-05 09:55:27 -0700

    Initial rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eventsoverflow::Application.config.secret_token = 'a9432203c9af9830ba047ff6ad23ae697d29964667f9f14c62888b9291b68697323109784db6ffdd25a1b732faa716234d7647e8e07acb94856bcd94c0cf84e1'
