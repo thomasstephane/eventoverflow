04ffd36dd1d6d4e341f742116a8263d0ba3997aa
Stephane Thomas <thomas.stephn@gmail.com>
2013-06-05 13:44:00 -0700

    Changed event controller spec nameevent_spec.rb -> events_controller_spec.rb Moved tests in appropriate spec file -from model to controller-

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -2,32 +2,4 @@ require 'spec_helper'
 
 describe "Event" do
 
-  let!(:event) {create(:event, :title => "my event", :description => "my event description")}
-
-  context "when create" do
-    it "should have a title" do
-      visit events_path
-      fill_in 'description', with: "my event description"
-      find("input[type='submit']").click
-      page.should_not have_content 'success'
-    end
-
-    it "should have a description" do
-      visit event_path
-      fill_in 'title', with: "my event"
-      find("input[type='submit']").click
-      page.should_not have_content 'success'
-    end
-  end
-
-  context "when view" do
-    it "should have the event title" do
-      visit event_path(event)
-      page.should have event.title
-    end
-    it "should have the event description" do
-      visit event_path(event)
-      page.should have event.description
-    end
-  end
 end
