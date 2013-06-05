7b7b64d9b53ad0699972bd8ea59d1a20d9b59650
Allen Lee <allenwlee@Allens-MacBook-Pro.local>
2013-06-05 11:31:56 -0700

    more changes to create events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,3 +1,30 @@
 class EventsController < ActionController::Base
   protect_from_forgery
+
+  def index
+    @events = Event.all
+  end
+
+  def create
+    @event = Event.new(params[:event])
+    @event.save
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
