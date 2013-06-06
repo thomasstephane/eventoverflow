2a08314b4e8b88ae8f8c7bae0552d0cef227b85e
Apprentice <apprentice@devbootcamp.com>
2013-06-06 11:49:10 -0700

    uncommented the User associations ...oops

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
-  # has_many :created_events, :class_name => 'Event'
+  has_many :created_events, :class_name => 'Event'
   has_many :comments
-  # has_many :commented_events, :through => :comments, :class_name => "Event", :source => :event
+  has_many :commented_events, :through => :comments, :class_name => "Event", :source => :event
 
   validates :username, :presence => true
 end
