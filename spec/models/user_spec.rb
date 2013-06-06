6d7654b69ec0d67eb9e7f523850db6d08d5b8017
dbcapprentice <dbcapprentice@dbc.com>
2013-06-05 17:44:03 -0700

    fixed rspec for user model added model validation for the presensce of username

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,17 +1,15 @@
 require 'spec_helper'
 
-describe User do 
+describe 'User' do 
   context "is instantiating" do
     it "should have a username" do
-      pending
-      # user = User.create(:password => '1234')
-      # expect(user.username).to be_invalid
+      user = User.create(password: '1234')
+      expect(user).to be_invalid
     end
 
     it "should have a password" do
-      pending
-      # user = User.create(:username => 'test')
-      # expect(user.password).to be_invalid
+      user = User.create(username: 'test')
+      expect(user).to be_invalid
     end
   end
 end
