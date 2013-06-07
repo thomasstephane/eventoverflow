e3767e54ed048608df6daf059e5699996732d2cd
Stephane Thomas <thomas.stephn@gmail.com>
2013-06-07 12:07:07 -0700

    Added new field for admin permissions Don't forget ti run db:migrate and db:test:prepare

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130607044914) do
+ActiveRecord::Schema.define(:version => 20130607190431) do
 
   create_table "comments", :force => true do |t|
     t.integer  "user_id"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(:version => 20130607044914) do
     t.string   "password_digest"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+    t.boolean  "admin"
   end
 
   create_table "votes", :force => true do |t|
