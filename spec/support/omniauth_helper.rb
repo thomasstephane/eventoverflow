280799550e8f405cab21c46ce2feac5a1520d742
Ken Rettberg <kenrettberg@gmail.com>
2013-06-07 23:12:13 -0700

    Removed unneccesary methods, they were redundant modified:   app/controllers/session_controller.rb
    Added tests for logging in with google
      modified:   spec/features/user_spec.rb
    
    Created a stub and mock to implement google login tests
    	new file:   spec/support/omniauth_helper.rb

---
+++ spec/support/omniauth_helper.rb
@@ -0,0 +1,19 @@
+OmniAuth.config.mock_auth[:google] = OmniAuth::AuthHash.new(
+    {
+        :provider => "google_oauth2",
+        :uid => "123456789",
+        :info => {
+            :name => "John Doe",
+            :email => "john@company_name.com",
+            :first_name => "John",
+            :last_name => "Doe",
+            :image => "https://lh3.googleusercontent.com/url/photo.jpg"
+        },
+        :credentials => {
+            :token => "token",
+            :refresh_token => "another_token",
+            :expires_at => 1354920555,
+            :expires => true
+        }
+    }
+)
