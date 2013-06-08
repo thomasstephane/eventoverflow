010ad5cfa2b5a6d46a70e4a1af06e3ef80e891f1
Stephane Thomas <thomas.stephn@gmail.com>
2013-06-08 14:16:04 -0700

    Corrected the rspec error due to toggled message app/assets/javascripts/comment.js
    Edit views to avoid error message when redirection
    app/views/events/_links.html.erb
    app/views/events/show.html.erb

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $('.comment-add').on('click', function(event){
     event.preventDefault();
-    $(this).parent().find('form:first').toggle("slow");
+    $(this).parent().find('form.new_comment:first').toggle("slow");
   });
 });
