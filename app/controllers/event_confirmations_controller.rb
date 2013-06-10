a2820552715a269d5c7fab4fe634903c4dd20269
Stephane Thomas <thomas.stephn@gmail.com>
2013-06-09 20:08:40 -0700

    Added case click without any radio button checked app/controllers/event_confirmations_controller.rb

--- app/controllers/event_confirmations_controller.rb
+++ app/controllers/event_confirmations_controller.rb
@@ -2,10 +2,12 @@ class EventConfirmationsController < ApplicationController
 
   def update
     @event = Event.find(params[:event_id])
-    @existing_decision = EventConfirmation.find_by_user_id_and_event_id(current_user.id, @event.id)
-    @event_confirmation = @existing_decision ? @existing_decision : EventConfirmation.new(:user_id => current_user.id, :event_id => @event.id)
-    @event_confirmation.decision = params[:event_confirmation][:decision]
-    @event_confirmation.save
+    if params[:event_confirmation]
+      @existing_decision = EventConfirmation.find_by_user_id_and_event_id(current_user.id, @event.id)
+      @event_confirmation = @existing_decision ? @existing_decision : EventConfirmation.new(:user_id => current_user.id, :event_id => @event.id)
+      @event_confirmation.decision = params[:event_confirmation][:decision]
+      @event_confirmation.save
+    end
     redirect_to event_path(@event)
   end
 
